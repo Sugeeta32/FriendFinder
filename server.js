fd56791cf12bdc8d13792c49de5c1a60b28981d8
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-11-21 12:51:46 -0500

    PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express= require("express");
 var bodyParser= require("body-parser");
 var path = require("path");
 
-var PORT = process.env.port||8080;
+var PORT = process.env.PORT||8080;
 
 var app = express();
 
@@ -12,19 +12,12 @@ app.use(bodyParser.urlencoded({extended: true})) ;
 app.use(bodyParser.json({ type: "application/**json"}));
 
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
